f5d9021850886261e88352af67bbacc2e447493c
victoriam <vicky.maron@gmail.com>
2020-02-06 10:20:07 -0300

    desimplementacion de interface

--- src/main/java/ar/com/ada/maven/model/dao/CountryDAO.java
+++ src/main/java/ar/com/ada/maven/model/dao/CountryDAO.java
@@ -12,15 +12,18 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-public class CountryDAO implements Dao<CountryDTO>{
+public class CountryDAO {
 
     private Boolean willCloseConnection = true;
 
-    public CountryDAO(){}
-    public CountryDAO(Boolean willCloseConnection) {this.willCloseConnection = willCloseConnection;}
+    public CountryDAO() {
+    }
+
+    public CountryDAO(Boolean willCloseConnection) {
+        this.willCloseConnection = willCloseConnection;
+    }
 
 
-    @Override
     public Collection<CountryDTO> findAll(int limit, int offset) {
         String sql = "SELECT * FROM Country LIMIT ? OFFSET ?";
         List<CountryDTO> countries = new ArrayList<>();
@@ -42,7 +45,7 @@ public class CountryDAO implements Dao<CountryDTO>{
         return countries;
     }
 
-    @Override
+
     public CountryDTO findById(Integer id) {
         String sql = "SELECT * FROM Country WHERE id = ?";
         CountryDTO country = null;
@@ -62,66 +65,4 @@ public class CountryDAO implements Dao<CountryDTO>{
         return country;
     }
 
-    @Override
-    public Boolean save(CountryDTO countryDTO) {
-        String sql = "INSERT INTO Country (name, code) values (?, ?)";
-        int affectatedRows = 0;
-        try {
-            Connection connection = DBConnection.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setString(1, countryDTO.getName());
-            preparedStatement.setString(2, countryDTO.getCode());
-            affectatedRows = preparedStatement.executeUpdate();
-            connection.close();
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        return affectatedRows == 1;
-    }
-
-    @Override
-    public Boolean update(CountryDTO countryDTO, Integer id) {
-        String sql = "UPDATE Country SET name = ?, code = ? WHERE id = ?";
-        int hasUpdate = 0;
-
-        CountryDTO countryDB = findById(id);
-
-        try {
-            Connection connection = DBConnection.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setString(1, countryDTO.getName());
-            preparedStatement.setString(2, countryDTO.getCode());
-            preparedStatement.setInt(3, id);
-
-            if (!(countryDTO.getName().equals(countryDB.getName()) &&
-                    countryDTO.getCode().equals(countryDB.getCode())));
-            hasUpdate = preparedStatement.executeUpdate();
-            connection.close();
-        } catch (Exception e) {
-            System.out.println("CONNECTION ERROR: " + e.getMessage());
-        }
-        return hasUpdate == 1;
-    }
-
-    @Override
-    public Boolean delete(Integer id) {
-        String sql = "DELETE FROM Country WHERE id = ?";
-        int hasDelete = 0;
-
-        try {
-            Connection connection = DBConnection.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setInt(1, id);
-            hasDelete = preparedStatement.executeUpdate();
-        } catch (Exception e) {
-            System.out.println("CONNECTION ERROR: " + e.getMessage());
-        }
-        return hasDelete == 1;
-    }
 }
