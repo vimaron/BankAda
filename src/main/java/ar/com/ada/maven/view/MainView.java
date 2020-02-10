a9d28feea8b94a7e70ad0cbea48b6bded5ee76f3
victoriam <vicky.maron@gmail.com>
2020-02-10 20:11:00 -0300

    main view

---
+++ src/main/java/ar/com/ada/maven/view/MainView.java
@@ -0,0 +1,27 @@
+package ar.com.ada.maven.view;
+
+import ar.com.ada.maven.utils.ScannerSingleton;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class MainView {
+    public int selectOption(){
+        System.out.println("Welcome to Corrupt Bank");
+        System.out.println("Selecciones una opcion: \n 1.Cliente \n 2.Cuentas \n 3.Salir");
+
+        Scanner key = ScannerSingleton.getInstance();
+
+
+        while (true){
+            try {
+                int choice = key.nextInt();
+                return choice;
+            } catch (InputMismatchException e){
+                System.out.println("El caracter ingresado es erroneo");
+                key.next();
+            }
+        }
+    }
+ }
+
