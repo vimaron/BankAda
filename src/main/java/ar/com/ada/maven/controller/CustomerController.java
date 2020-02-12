086d0459052ee1452484c295fb7a1075d6a85cb7
victoriam <vicky.maron@gmail.com>
2020-02-12 17:44:36 -0300

    customersList

--- src/main/java/ar/com/ada/maven/controller/CustomerController.java
+++ src/main/java/ar/com/ada/maven/controller/CustomerController.java
@@ -3,6 +3,7 @@ package ar.com.ada.maven.controller;
 import ar.com.ada.maven.model.dao.CustomerDAO;
 import ar.com.ada.maven.model.dto.CustomerDTO;
 import ar.com.ada.maven.view.CustomerView;
+import ar.com.ada.maven.view.MainView;
 
 import java.util.List;
 
@@ -16,8 +17,10 @@ public class CustomerController {
             int varWhile = view.customerMenuSelectOption();
             switch (varWhile){
                 case 1:
-                    customerList();
+                    customersList();
                     break;
+                case 2:
+                    createNewCustomer();
                 default:
                     System.out.println("Se debe seleccionar una opcion valida");
             }
@@ -25,8 +28,61 @@ public class CustomerController {
         }
     }
 
-    private static void customerList(){
-        List<CustomerDTO> customerFindAll = customerDAO.findAll();
-        view.printAllCustomers(customerFindAll);
+
+    private static void customersList() {
+        listCustomersPerPage(null, true);
     }
+
+    static int listCustomersPerPage(String optionSelectEdithOrDelete, boolean showHeader) {
+        int limit = 4, currentPage = 0, totalCustomers, totalPages, customerIdSelected = 0;
+        List<CustomerDTO> customers;
+        List<String> paginator;
+        boolean shouldGetOut = false;
+
+        while (!shouldGetOut) {
+            totalCustomers = customerDAO.getTotalCustomers(); //crear metodo en customerDAO
+            totalPages = (int) Math.ceil((double) totalCustomers / limit);
+            paginator = Paginator.buildPaginator(currentPage, totalPages); //importar cambios de develor e importar clase
+
+            customers = customerDAO.findAll(limit, currentPage * limit);
+            String choice = view.printCustomersPerPage(customers, paginator, optionSelectEdithOrDelete, showHeader);
+
+            switch (choice) {
+                case "i":
+                case "I":
+                    currentPage = 0;
+                    break;
+                case "a":
+                case "A":
+                    if (currentPage > 0) currentPage--;
+                    break;
+                case "s":
+                case "S":
+                    if (currentPage + 1 < totalPages) currentPage++;
+                    break;
+                case "u":
+                case "U":
+                    currentPage = totalPages - 1;
+                    break;
+                case "e":
+                case "E":
+                    if (optionSelectEdithOrDelete != null) {
+                        customerIdSelected = view.customerIdSelected(optionSelectEdithOrDelete);
+                        shouldGetOut = true;
+                    }
+                    break;
+                case "q":
+                case "Q":
+                    shouldGetOut = true;
+                    break;
+                default:
+                    if (choice.matches("^-?\\d+$")) {
+                        int page = Integer.parseInt(choice);
+                        if (page > 0 && page <= totalPages) currentPage = page - 1;
+                    } else MainView.invalidData();
+            }
+        }
+        return customerIdSelected;
+    }
+
 }
--- src/main/java/ar/com/ada/maven/view/CustomerView.java
+++ src/main/java/ar/com/ada/maven/view/CustomerView.java
@@ -2,15 +2,18 @@ package ar.com.ada.maven.view;
 
 import ar.com.ada.maven.model.dto.CustomerDTO;
 import ar.com.ada.maven.utils.ScannerSingleton;
+import jdk.internal.jline.internal.Ansi;
 
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
+import static ar.com.ada.maven.utils.ScannerSingleton.keyboard;
+
 public class CustomerView {
     public int customerMenuSelectOption(){
         System.out.println("Corrupt Bank :: Modulo Cliente");
-        System.out.println("Seleccione una opcion: \n 1.Lista \n 2.Agregar \n 3.Editar \n 4.Eliminar \n 5.Salir");
+        System.out.println("Seleccione una opcion: \n 1.Listar \n 2.Agregar \n 3.Editar \n 4.Eliminar \n 5.Salir");
 
         Scanner keyboard = ScannerSingleton.getInstance();
 
@@ -35,5 +38,64 @@ public class CustomerView {
         });
         ScannerSingleton.pressEnterKeyToContinue();
     }
+
+    public String printCustomersPerPage(List<CustomerDTO> customers, List<String> paginator,
+                                        String optionEdithOrDelete, boolean showHeader) {
+        if (showHeader) {
+            System.out.println("\n+----------------------------------------------------------------+");
+            System.out.println("\t  Corrupt Bank :: Modulo Clientes :: Lista Cliente");
+            System.out.println("+----------------------------------------------------------------+\n");
+        }
+
+        if (optionEdithOrDelete != null && !optionEdithOrDelete.isEmpty())
+            paginator.set(paginator.size() - 2, optionEdithOrDelete);
+
+        System.out.println("\n+----------------------------------------------------------------+");
+        paginator.forEach(page -> System.out.print(page + " "));
+        System.out.println("\n+----------------------------------------------------------------+\n");
+
+        while (true) {
+            try {
+                System.out.print("? ");
+                String name = keyboard.nextLine().trim();
+                while (!name.matches("^[0-9IiAaSsUuEeqQ]+$") && !name.isEmpty()) {
+                    MainView.invalidData();
+                    System.out.print("? ");
+                    name = keyboard.nextLine();
+                }
+                return name;
+            } catch (InputMismatchException e) {
+                MainView.invalidData();
+                keyboard.next();
+            }
+        }
+    }
+
+    public int customerIdSelected(String actionOption) {
+        switch (actionOption) {
+            case "[" + Ansi.CYAN + "E" + Ansi.RESET + "ditar]":
+                actionOption = "editar";
+                break;
+            case "[" + Ansi.CYAN + "E" + Ansi.RESET + "liminar]":
+                actionOption = "eliminar";
+                break;
+            case "[" + Ansi.CYAN + "E" + Ansi.RESET + "lejir]":
+                actionOption = "elejir";
+                break;
+        }
+        System.out.println("Ingrese el numero de ID del cliente para " + actionOption + " ó 0 para cancelar: \n");
+
+        while (true) {
+            try {
+                System.out.print("? ");
+                int choice = keyboard.nextInt();
+                return choice;
+            } catch (InputMismatchException e) {
+                MainView.invalidData();
+                keyboard.next();
+            }
+        }
+    }
+
 
 }
