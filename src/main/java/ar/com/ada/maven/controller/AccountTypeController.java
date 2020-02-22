c7b6f4e7bac60281a8d575a3061348067bc7f4a4
Daiana Delvalle <52934488+daianadelvalle@users.noreply.github.com>
2020-02-21 22:02:56 -0300

    vista y controller de accountType

---
+++ src/main/java/ar/com/ada/maven/controller/AccountTypeController.java
@@ -0,0 +1,165 @@
+package ar.com.ada.maven.controller;
+
+import ar.com.ada.maven.model.dao.AccountTypeDAO;
+import ar.com.ada.maven.model.dto.AccountTypeDTO;
+import ar.com.ada.maven.utils.Paginator;
+import ar.com.ada.maven.view.AccountTypeView;
+import ar.com.ada.maven.view.MainView;
+
+import java.util.List;
+
+public class AccountTypeController {
+
+    private static AccountTypeView view = new AccountTypeView();
+    private static AccountTypeDAO accountTypeDAO = new AccountTypeDAO(false);
+
+    static void init() {
+        boolean shouldGetOut = false;
+
+        while (!shouldGetOut) {
+            Integer option = view.accountTypeMenuSelectOption();
+            switch (option) {
+                case 1:
+                    listAllAccountsType();
+                    break;
+                case 2:
+                    createNewAccountType();
+                    break;
+                case 3:
+                    deleteSelectedAccountType();
+                    break;
+                case 5:
+                    shouldGetOut = true;
+                    break;
+                default:
+                    MainView.chooseValidOption(); //revisar este metodo
+            }
+        }
+    }
+
+    private static void listAllAccountsType() {
+        listAccountsTypePerPage(null, true);
+    }
+
+    private static void createNewAccountType() {
+        String nameNewAccountType = view.getNameNewAccountType();
+
+        if (!nameNewAccountType.isEmpty()) {
+
+            AccountTypeDTO newAccountType = new AccountTypeDTO(nameNewAccountType);
+            AccountTypeDTO byName = accountTypeDAO.findByName(nameNewAccountType);
+
+            if (byName != null && byName.equals(newAccountType)) {
+                view.accountTypeAlreadyExists(newAccountType.getName());
+            } else {
+                Boolean isSaved = accountTypeDAO.save(newAccountType);
+                if (isSaved)
+                    view.showNewAccountType(newAccountType.getName());
+            }
+        } else {
+            view.newAccountTypeCanceled();
+        }
+    }
+
+    static int listAccountsTypePerPage(String optionSelectEdithOrDelete, boolean showHeader) {
+        int limit = 4, currentPage = 0, totalAccountsType, totalPages, accountTypeIdSelected = 0;
+        List<AccountTypeDTO> accountsType;
+        List<String> paginator;
+        boolean shouldGetOut = false;
+
+        while (!shouldGetOut) {
+            totalAccountsType = accountTypeDAO.getTotalAccountsType();
+            totalPages = (int) Math.ceil((double) totalAccountsType / limit);
+            paginator = Paginator.buildPaginator(currentPage, totalPages);
+
+            accountsType = accountTypeDAO.findAll(limit, currentPage * limit);
+            String choice = view.printAccountsTypePerPage(accountsType, paginator, optionSelectEdithOrDelete, showHeader);
+
+            switch (choice) {
+                case "i":
+                case "I":
+                    currentPage = 0;
+                    break;
+                case "a":
+                case "A":
+                    if (currentPage > 0) currentPage--;
+                    break;
+                case "s":
+                case "S":
+                    if (currentPage + 1 < totalPages) currentPage++;
+                    break;
+                case "u":
+                case "U":
+                    currentPage = totalPages - 1;
+                    break;
+                case "e":
+                case "E":
+                    if (optionSelectEdithOrDelete != null) {
+                        accountTypeIdSelected = view.accountTypeIdSelected(optionSelectEdithOrDelete);
+                        shouldGetOut = true;
+                    }
+                    break;
+                case "q":
+                case "Q":
+                    shouldGetOut = true;
+                    break;
+                default:
+                    if (choice.matches("^-?\\d+$")) {
+                        int page = Integer.parseInt(choice);
+                        if (page > 0 && page <= totalPages) currentPage = page - 1;
+                    } else MainView.chooseValidOption();
+            }
+        }
+        return accountTypeIdSelected;
+    }
+    private static void editSelectedAccountType(int id) {
+        AccountTypeDTO accountTypeById = accountTypeDAO.findById(id);
+        if (accountTypeById != null) {
+            String nameToUpdate = view.getNameToUpdate(accountTypeById);
+            if (!nameToUpdate.isEmpty()) {
+                accountTypeDAO.findByName(nameToUpdate);
+                accountTypeById.setName(nameToUpdate);
+
+                Boolean isSaved = accountTypeDAO.update(accountTypeById, id);
+
+                if (isSaved)
+                    view.showUpdateAccountType(accountTypeById.getName());
+            } else
+                view.updateAccountTypeCanceled();
+        } else {
+            view.accountTypeNotExist(id);
+            int accountTypeIdSelected = view.accountTypeIdSelected("Editar");
+            if (accountTypeIdSelected != 0)
+                editSelectedAccountType(accountTypeIdSelected);
+            else
+                view.updateAccountTypeCanceled();
+        }
+    }
+
+    private static void deleteSelectedAccountType(int id) {
+        AccountTypeDTO accountTypeDTO = accountTypeDAO.findById(id);
+        if (accountTypeDTO != null) {
+            Boolean toDelete = view.getResponseToDelete(accountTypeDTO);
+            if (toDelete) {
+
+                Boolean isDelete = accountTypeDAO.delete(id);
+
+                if (isDelete)
+                    view.showDeleteAccountType(accountTypeDTO.getName());
+            } else
+                view.deleteAccountTypeCanceled();
+        } else {
+            view.accountTypeNotExist(id);
+            int accountTypeIdSelected = view.accountTypeIdSelected("Eliminar");
+            if (accountTypeIdSelected != 0)
+                deleteSelectedAccountType(accountTypeIdSelected);
+            else
+                view.deleteAccountTypeCanceled();
+        }
+    }
+
+
+
+
+
+}
--- src/main/java/ar/com/ada/maven/model/dto/AccountTypeDTO.java
+++ src/main/java/ar/com/ada/maven/model/dto/AccountTypeDTO.java
@@ -3,7 +3,7 @@ package ar.com.ada.maven.model.dto;
 import java.util.Objects;
 
 public class AccountTypeDTO {
-    private int id;
+    private Integer id;
     private String name;
 
     public AccountTypeDTO(){}
@@ -13,8 +13,8 @@ public class AccountTypeDTO {
         this.name=name;
     }
 
-    public int getId(){return id;}
-    public void setId(int id){this.id = id;}
+    public Integer getId(){return id;}
+    public void setId(Integer id){this.id = id;}
     public String getName(){return name;}
     public void setName(String name){this.name=name;}
 
